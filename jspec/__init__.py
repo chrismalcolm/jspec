88dbde29125872d27b643414bfde2c3edb963331
chrismalcolm <chris.c.malcolm.96@gmail.com>
2021-07-08 22:09:08 +0100

    Update version

--- jspec/__init__.py
+++ jspec/__init__.py
@@ -1,15 +1,8 @@
 """Jspec Module"""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from jspec.parser import load, loads
-from jspec.constants import (
-    JSPEC_ELLIPSIS_SUBSTITUTIONS,
-    JSPEC_OBJECT_ITEMS_SUBSTITUTION, 
-    JSPEC_OBJECT_TEMPLATE_VALUE, 
-    JSPEC_ARRAY_ELEMENT_SUBSTITUTION, 
-    JSPEC_ARRAY_TEMPLATE_VALUE
-)
 from jspec.error import JSpecError, JSpecLoadError
 from jspec.jregex import JSpec
 from jspec.result import Result
