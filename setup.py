a7c909db4ad5e6388d56918dae8c929be696f869
chrismalcolm <chris.c.malcolm.96@gmail.com>
2021-07-09 23:56:12 +0100

    Update setuo.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Christopher Malcolm",
     author_email="chris.c.malcolm.96@gmail.com",
     name="jspec",
-    version="0.1.0",
+    version="1.0.0",
     description="Jspec is a tool that can be used to check the regex and structure of JSON",
     long_description_content_type="text/markdown",
     long_description=long_description,
